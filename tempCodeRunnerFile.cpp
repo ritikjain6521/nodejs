e1544e01175bc970f2c0e2af5c32aa5e8f9c4dfb
unknown <ritikjain6224@email.com>
2024-04-27 15:12:21 +0530

    html tutorial

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b,sum;
+
+  cout<<"enter the value of a :";
+  cin>>a;
+  cout<<"enter the value of b:";
+  cin>>b;
+   sum=a+b;
+
+   cout<<"sum of the numbers:">>sum;
+
+
+
+
+
+
+
+}
